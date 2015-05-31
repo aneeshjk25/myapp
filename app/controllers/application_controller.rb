0904e818d5e640018730ca9b2935ded74bc62f0b
aneeshjk13 <kallarakkal@hotmail.com>
2015-05-31 23:55:18 +0530

    renamed the app from 'myapp' to mychartapp, also saved some changes to application controller which had been left behind

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,6 @@ class ApplicationController < ActionController::Base
   # function expect url which responds with jsonp data
   # it appends the name 'callback' to url and return json strin
   # passing true return hash object
-  def getJsonFromJsonp url, parse = false
-  	require 'net/http'
-	require 'json'
-	url = url+'?callback=callback'
-	uri = URI.parse(url)
-	jsonp = Net::HTTP.get(uri)
-	jsonp.gsub!(/^.*callback\(/, '') # removes the comment and callback function from the start of the string
-	hash = jsonp.gsub!(/\)$/, '') # removes the end of the callback function
-	#jsonp.gsub!(/(\w+):/, '"\1":')
-	if parse
-		hash = JSON.parse(jsonp)
-	end
-	hash
-  end
-
   def getJson url, parse = false
   	require 'net/http'
 	require 'json'
@@ -38,5 +23,7 @@ class ApplicationController < ActionController::Base
 	end
 	hash
   end
+
+  
 
 end

664e331df9c44f70205a9a4b45fcbc01aec91ae2
aneeshjk13 <kallarakkal@hotmail.com>
2015-06-01 01:47:06 +0530

    added heroku logging for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,10 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  # for heroku logging
+  $stdout.sync = true
+  config.logger = Logger.new(STDOUT)
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 

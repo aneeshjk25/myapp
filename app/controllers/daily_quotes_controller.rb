63bcfad2a08399ebabd156013cba098b356c0dfa
aneeshjk13 <kallarakkal@hotmail.com>
2015-06-01 21:43:11 +0530

    added puma for production only, daily updates import only insert records which when needed, duplicate record avoided

--- app/controllers/daily_quotes_controller.rb
+++ app/controllers/daily_quotes_controller.rb
@@ -25,15 +25,22 @@ class DailyQuotesController < ApplicationController
 		def _import collection_of_quotes,company
 			unless collection_of_quotes['series'] == nil
 				collection_of_quotes['series'].each do |series|
-					daily_quote = DailyQuote.new()
-					daily_quote.company_id 	= company.id
-					daily_quote.quote_date 	= series['Date']
-					daily_quote.low_price 	= series['low']
-					daily_quote.high_price 	= series['high']
-					daily_quote.open_price 	= series['open']
-					daily_quote.close_price = series['close']
-					daily_quote.volume 		= series['volume']
-					daily_quote.save
+					daily_quote = DailyQuote.find_by("quote_date = '?' AND company_id = ? ",series['Date'],company.id)
+					if daily_quote == nil
+						# save new quote
+						daily_quote = DailyQuote.new()
+						daily_quote.company_id 	= company.id
+						daily_quote.quote_date 	= series['Date']
+						daily_quote.low_price 	= series['low']
+						daily_quote.high_price 	= series['high']
+						daily_quote.open_price 	= series['open']
+						daily_quote.close_price = series['close']
+						daily_quote.volume 		= series['volume']
+						daily_quote.save
+					else
+						#quote already exists, do nothing
+					end
+					
 				end
 			end
 		end

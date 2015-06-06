668daa2bda2372fa044c30169d74351ed2d04d8c
aneeshjk13 <kallarakkal@hotmail.com>
2015-06-06 21:09:53 +0530

    removed heroku logging for openshift

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,10 +54,6 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # for heroku logging
-  $stdout.sync = true
-  config.logger = Logger.new(STDOUT)
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 

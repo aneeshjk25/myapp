243d5ce09cd6c0f1bcc0c50df96c113d2cfb7691
aneeshjk13 <kallarakkal@hotmail.com>
2015-06-16 21:56:59 +0530

    analytics report almost complete, gives the high for interval properly and needs some work for its incease thrighout  the day

--- app/services/analytics.rb
+++ app/services/analytics.rb
@@ -7,6 +7,7 @@ class Analytics
 
 	def bull_report
 		companies = Company.active
+		#companies = Company.where(symbol: 'BPCl')
 		dates 	  = Quote.get_distinct_dates
 		count = 1
 		dates.each do |date|
@@ -16,42 +17,46 @@ class Analytics
 			best_performing_stock_till_interval = nil
 			companies.each do |company|
 				highest_quote = Quote.top Quote.quote_types[:minute],date.quote_date,company.id
-				day_quote 	  = Quote.get_by_date date.quote_date
+				day_quote 	  = Quote.get_by_date date.quote_date,company.id
 
-				unless highest_quote == nil
+				if highest_quote != nil && day_quote != nil
 					# price has appreciated over open
 					if day_quote.open_price <= highest_quote.high_price
 						#if stock is best performing save it
 						percentage_rise = percentage_rise day_quote.open_price,highest_quote.high_price
-						if highest_quote.high_price > best_performing_number_till_interval 
+						if percentage_rise > best_performing_number_till_interval 
 							best_performing_number_till_interval = percentage_rise
 							best_performing_stock_till_interval  = company
 							#print "Putting this for  #{date.quote_date} is #{best_performing_stock_till_interval.company_name}\n"
 						end
 					end
 
-					# the price has appreciated , over the high of 10:30 and stock is best performing
-					if ( (highest_quote.high_price < day_quote.high_price) && best_performing_stock_till_interval != nil && (company.id == best_performing_stock_till_interval.id))
-						# now calculate percentage rise
-						percentage_rise = percentage_rise (highest_quote.high_price,day_quote.high_price)
-=begin
-
-						percentage_rise = percentage_rise highest_quote.high_price,day_quote.high_price
-						if percentage_rise > best_performing_number
-							best_performing_number = percentage_rise
-							best_performing_stock  = company
-						end
-
-=end
-					# no price appreciation , after being the best performing till interval
-					else
-						# do something here
+					# stock is best performing
+					if best_performing_stock_till_interval != nil && (company.id == best_performing_stock_till_interval.id)
+						# the price has appreciated , over the high of 10:30
+						if ( (highest_quote.high_price < day_quote.high_price) )
+							# now calculate percentage rise
+							percentage_rise = percentage_rise (highest_quote.high_price,day_quote.high_price)
+							if percentage_rise > best_performing_number
+								best_performing_number = percentage_rise
+								best_performing_stock  = company
+							end
+						# no price appreciation , after being the best performing till interval
+						else
+							# do something here
+							print "failure::"
+							best_performing_number = 0
+							best_performing_stock = Object.new
+							best_performing_stock.company_name = 'NA'
+						end						
 					end
+
 				end
 		
 			end # end company loop
 			unless best_performing_stock_till_interval == nil 
-				print "The best performing stock for #{date.quote_date} is #{best_performing_stock_till_interval.company_name} rise is #{best_performing_number_till_interval}\n"
+				print "The best performing stock for interval #{date.quote_date} is #{best_performing_stock_till_interval.company_name} rise is #{best_performing_number_till_interval}\n"
+				print "The stock permormance #{date.quote_date} is #{best_performing_stock.company_name} rise is #{best_performing_number}\n\n"
 			 else
 			 	print "NA\n"
 			end
